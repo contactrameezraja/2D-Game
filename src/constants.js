08a6e77b9ebbdf1f1a844974fea8448063e7f289
Rameez Raja <135865477+contactrameezraja@users.noreply.github.com>
2024-05-01 18:25:21 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I spend a lot of time infront of the computer, with double or triple monitors. I mostly work on data analysis and business intelligence applications as well as coding projects and app development. 
-    I've made a couple of games in Pyhton and Javascript/Typescript. I also like Golang. Anyway regardless of the language, I just like programming.
+    I've made a couple of games in Pyhton and Javascript/Typescript. I also like Golang. Anyway regardless of the language, I just like programming.',
     
   "cs-degree": `This is my MS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I rarely get time to watch any TV shows but if I do I like to relax here and watch YouTube.',
