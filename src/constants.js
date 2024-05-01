9f0431f31fbb8f08d1a3588e0579a0ee4008ae4a
Rameez Raja <135865477+contactrameezraja@users.noreply.github.com>
2024-05-01 18:33:35 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,15 +5,13 @@ export const dialogueData = {
     I've made a couple of games in Pyhton and Javascript/Typescript. I also like Golang. Anyway regardless of the language, I just like programming.`,
     
   "cs-degree": `This is my MS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I rarely get time to watch any TV shows but if I do I like to relax here and watch YouTube.`,
+  "sofa-table": `That's my sofa. I rarely get time to watch any TV shows but if I do I like to relax here and watch YouTube or read my all time favorite is On Writing by Stephen King.`,
   tv: `That's my TV. I've been watching tech youtubers a lot`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk where most of my projects and work take place. Contact me at contactrameezraja@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  Kaboom.js by watching this tutorial <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
 };
