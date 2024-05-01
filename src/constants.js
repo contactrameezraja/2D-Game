e53a5b8bc261de0c33e7c0234edca530ab6b8560
Rameez Raja <135865477+contactrameezraja@users.noreply.github.com>
2024-05-01 18:22:08 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,9 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `This is my PC. I spend a lot of time infront of the computer, with double or triple monitors. I mostly work on data analysis and business intelligence applications as well as coding projects and app development. 
     I've made a couple of games in Pyhton and Javascript/Typescript. I also like Golang. Anyway regardless of the language, I just like programming.
-    Here is my git <https://github.com/contactrameezraja`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    
+  "cs-degree": `This is my MS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I rarely get time to watch any TV shows but if I do I like to relax here and watch YouTube. 
   I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
   You'll learn how I built this portfolio you're currently playing through!`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
